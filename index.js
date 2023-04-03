0a45b849de381ba64b841fa42262f90acc34fe0a
sigmapng <riabkoyuliia@gmail.com>
2023-04-03 17:02:22 +0300

    dark and light mode

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const imgIcon = document.getElementById("mode-icon");
+const logos = document.getElementsByClassName("social-media-logo");
+
+imgIcon.onclick = function () {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    imgIcon.src = "img/sun.png";
+    for (let i = 0; i < logos.length; i++) {
+      const logo = logos[i];
+      const originalSrc = logo.getAttribute("src");
+      const fileExtensiion = originalSrc.split(".").pop();
+      const newSrc = originalSrc.replace(
+        `.${fileExtensiion}`,
+        `-white.${fileExtensiion}`
+      );
+      logo.setAttribute("src", newSrc);
+    }
+  } else {
+    imgIcon.src = "img/moon.png";
+    for (let i = 0; i < logos.length; i++) {
+      const logo = logos[i];
+      const originalSrc = logo.getAttribute("src");
+      const fileExtensiion = originalSrc.split(".").pop();
+      const newSrc = originalSrc.replace(
+        `-white.${fileExtensiion}`,
+        `.${fileExtensiion}`
+      );
+      logo.setAttribute("src", newSrc);
+    }
+  }
+};
+
